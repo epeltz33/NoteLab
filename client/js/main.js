ac8216e82c46e325f06e499225bccf09ad6e5c02
epeltz33 <epeltz@optonline.net>
2024-12-04 17:22:34 -0500

    comments

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   var animationContainer = document.getElementById('lottie-container');
-
+// define the animation
   var animation = lottie.loadAnimation({
     container: animationContainer,
     renderer: 'svg',
